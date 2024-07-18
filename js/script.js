d5e3b186506931f1db66b46fa1e4c5d84379ce8b
iRishabhSingh <rishab18011@gmail.com>
2024-07-18 20:03:53 +0530

    Initial Commit of Calculator App

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const app = document.getElementById("app");
+const appIcon = document.getElementById("appIcon");
+
+function openApp() {
+  appIcon.style.display = "none";
+  app.style.display = "block";
+}
